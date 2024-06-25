85ee839a78bece10a75aaa268164ac14dbf1281e
Rafia Wahab <rafia.wahab@gmail.com>
2024-06-25 19:40:19 +0500

    third commit

---
+++ conversion.js
@@ -0,0 +1,6 @@
+let strValue ="Hi";
+console.log(typeof(strValue));
+let numValue =1;
+console.log(typeof(numValue));
+let booValue = true;
+console.log(typeof(booValue));
---
+++ json.js
@@ -0,0 +1,10 @@
+let Owner ={
+    firstname:"Rafia",
+    lastname:"Wahab",
+    brithdate: new Date("January 5,2012"),
+    title:"Owner",
+    salary:1000000000000000000000000000000
+};
+
+let jsonvalue = JSON.stringify(Owner);
+console.log(jsonvalue);
